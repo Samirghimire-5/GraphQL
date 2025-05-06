685da127f625b99a830e87991f9f9ef7cf33477f
Samirghimire-5 <samirghimire9841@gmail.com>
2025-05-06 21:28:22 +0545

    simple graphql schema and resolvers

--- src/index.js
+++ src/index.js
@@ -1,37 +1,53 @@
-const { buildSchema } = require("graphql");
 const { createHandler } = require("graphql-http/lib/use/express");
 const { ruruHTML } = require("ruru/server");
 const express = require("express");
-const port = 4000;
+const {
+  GraphQLObjectType,
+  GraphQLSchema,
+  GraphQLString,
+  GraphQLFloat,
+  GraphQLList,
+  GraphQLInt,
+  GraphQLNonNull,
+} = require("graphql");
 
-const schema = buildSchema(`
-  type Query {
-    quoteOfTheDay: String
-    random: Float!
-    rollThreeDice: [Int]
-  }
-  `);
-
-  const root = {
-    quoteOfTheDay: () => {
-      return Math.random() < 0.5 ? "Take it easy" : "Salvation lies within";
+// Construct a schema
+const schema = new GraphQLSchema({
+  query: new GraphQLObjectType({
+    name: "Query",
+    fields: {
+      rollDice: {
+        type: new GraphQLList(GraphQLFloat),
+        args: {
+          numDice: { type: new GraphQLNonNull(GraphQLInt) },
+          numSides: { type: GraphQLInt },
+        },
+        resolve: (_, { numDice, numSides }) => {
+          const output = [];
+          for (let i = 0; i < numDice; i++) {
+            output.push(1 + Math.floor(Math.random() * (numSides || 6)));
+          }
+          return output;
+        }
+      },
     },
-    random: () => {
-      return Math.random();
-    },
-    rollThreeDice: () => {
-      return [1, 2, 3].map(_ => 1 + Math.floor(Math.random() * 6));
-    },
-  };
+  }),
+});
 
 const app = express();
 
-app.all('/graphqlTesting', createHandler({
-  schema: schema,
-  rootValue: root,
-})
-)
-
-app.listen(port, () => {
-  console.log(`Server is running on port ${port}`);
+app.get("/graphql", (_req, res) => {
+  res.type = "html";
+  res.end(ruruHTML({ endpoint: "/graphql" }));
 });
+
+app.all(
+  "/graphql",
+  createHandler({
+    schema,
+  })
+);
+
+
+app.listen(4000);
+console.log("Running a GraphQL API server at localhost:4000/graphql");
